29dc2a0b85b3effd62b9b38da13cf0ed07d169c9
isharaU <isharauditha5@gmail.com>
2023-09-27 13:15:37 +0530

    pros

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import Upcomingweather from "./src/components/Upcomingweather";
 import Weather from "./src/components/Weather";
+import OurChild from "./src/components/OurChild";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Upcomingweather />
+      <OurChild message= {"Hello"}/>
+      <OurChild message= {"Greetings"}/>
+      <OurChild message= {"Goodbye"}/>
     </View>
   );
 };
--- src/components/Upcomingweather.js
+++ src/components/Upcomingweather.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { View, FlatList, Text, StyleSheet , Image } from 'react-native';
+import { View, FlatList, Text, StyleSheet , ImageBackground } from 'react-native';
 
 const data = [
   { id: '1', text: 'Item 1' },
@@ -10,42 +10,52 @@ const data = [
   { id: '5', text: 'Item 5' },
 ];
 
-const MyListComponent = () => {
+const Upcomingweather = () => {
   return (
-    <SafeAreaView style={styles.container}>
-        <Text>Upcoming Weather!</Text>    
-        <Image
+    <SafeAreaView style={styles.container}>    
+        <ImageBackground
             source = {require("./cloudy.jpg")}
-            style = {styles.image}
-        />
-        <FlatList
-            data={data}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item }) => (
-            <View style={styles.item}>
-                <Text>{item.text}</Text>
-            </View>
-            )}
-        />
-        </SafeAreaView>
+            style = {styles.image}>
+
+            <Text style={styles.head}>Upcoming Weather!</Text>
+            <FlatList
+                data={data}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                <View style={styles.item}>
+                    <Text>{item.text}</Text>
+                </View>
+                )}    
+            />
+        </ImageBackground>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 16,
   },
+
   item: {
     padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
+    flex: 0.1,
   },
+
+  head: {
+    paddingBottom: 20,
+    },
 
   image: {
-    width: 200,
-    height: 200,
+    paddingTop: 50,
+    paddingLeft: 20,
+    width: 350,
+    height: 800,
+    flex: 0.3,
   },
+  
 });
 
-export default MyListComponent;
+export default Upcomingweather;
